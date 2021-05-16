5ac1edabc452db39f620a9927340099c05c4eb51
Muthukumaran Kasiviswanathan <muthuk@microsoft.com>
2021-05-16 14:15:58 +0530

    Learning1

---
+++ LEARNING COMPUTER.py
@@ -0,0 +1,14 @@
+print("hello world")
+print("I am learning phython")
+print(" hi")
+
+place = input("which place are you staying in?")
+abc = input("what is your forename? ")
+xyz = input("what is your age")
+
+print("hello",abc)
+print("your Age is",xyz)
+
+print(abc, "is a child because her age is ", xyz)
+print(abc, "is your name", xyz, "is your AGE. You live in", place )
+
