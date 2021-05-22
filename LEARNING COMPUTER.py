424dd567414728f0e207717eed49ac82489c4f76
Muthukumaran Kasiviswanathan <muthuk@microsoft.com>
2021-05-22 14:55:24 +0530

    Numbers

--- LEARNING COMPUTER.py
+++ LEARNING COMPUTER.py
@@ -1,14 +1,34 @@
-print("hello world")
-print("I am learning phython")
-print(" hi")
+# print("hello world")
+# print("I am learning phython")
+# print(" hi")
 
-place = input("which place are you staying in?")
-abc = input("what is your forename? ")
-xyz = input("what is your age")
+# place = input("which place are you staying in?")
+# abc = input("what is your forename? ")
+# xyz = input("what is your age")
 
-print("hello",abc)
-print("your Age is",xyz)
+# print("hello",abc)
+# print("your Age is",xyz)
 
-print(abc, "is a child because her age is ", xyz)
-print(abc, "is your name", xyz, "is your AGE. You live in", place )
+# print(abc, "is a child because her age is ", xyz)
+# print(abc, "is your name", xyz, "is your AGE. You live in", place )
 
+# print(abc)
+
+# print ("ADDITION: 100 + 10 =", 100+10)
+# print ("ADDITION: 16+8 =", 16+8)
+# print (48*24)
+# print (45926/96)
+# print (392-147)
+
+# import random
+# number = random.randrange(13,200,1)
+# print (number)
+
+# print(random.randrange(100,200,1))
+# print(random.randrange(100,200,2))
+# print(random.randrange(100,200,3))
+# print(random.randrange(100,200,4))
+
+number = float(input("Type a whole number:  "))
+answer = number * 8
+print(number,"multiplied by 8 is",answer)
