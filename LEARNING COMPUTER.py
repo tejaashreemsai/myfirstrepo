1c6205c8ec65524f45d8474b39a8a1fd6cedd017
Muthukumaran Kasiviswanathan <muthuk@microsoft.com>
2021-08-14 15:20:53 +0530

    CONDITIONS

--- LEARNING COMPUTER.py
+++ LEARNING COMPUTER.py
@@ -29,6 +29,48 @@
 # print(random.randrange(100,200,3))
 # print(random.randrange(100,200,4))
 
-number = float(input("Type a whole number:  "))
-answer = number * 8
-print(number,"multiplied by 8 is",answer)
+# number = float(input("Type a whole number:  "))
+#answer = number * 8
+#print(number,"multiplied by 8 is",answer)
+
+# # Step1: get the number
+# mynumber = int (input ("enter a number: "))
+# # Step2: Check if number is divisible by 2
+# reminder = mynumber%2
+# # Step3: Print odd if number is not divisible, print even if number is divisible
+# if (reminder == 0):
+#     print("The number ", mynumber, "is even")
+# else:
+#     print("The number ", mynumber, "is odd")
+
+# # step 1 : get the number 
+# year = int ( input ( " enter a year"))
+# # step 2 ; check whether the number is divisible by 4
+# answer = year %4
+# if ( answer == 0 ): print (" the year is leap year ")
+# else: print (" the year is not leap year")
+
+
+
+
+mark = int ( input ( " enter a mark "))
+
+if ( mark > 90): print ( " grade +A")
+elif ( mark > 70 ): print ( " grade B+")
+elif ( mark > 40): print ( " grade C")
+else: print ( " grade D") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
